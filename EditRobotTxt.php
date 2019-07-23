3c084b08a7de44dde5efc6b8d98ff71727f459fe
Nicolas Barbey <223106@supinfo.com>
2019-07-23 12:06:52 +0200

    Auto generate robots table

--- EditRobotTxt.php
+++ EditRobotTxt.php
@@ -12,6 +12,11 @@
 
 namespace EditRobotTxt;
 
+use EditRobotTxt\Model\Robots;
+use EditRobotTxt\Model\RobotsQuery;
+use Propel\Runtime\Connection\ConnectionInterface;
+use Thelia\Install\Database;
+use Thelia\Model\LangQuery;
 use Thelia\Module\BaseModule;
 
 class EditRobotTxt extends BaseModule
@@ -19,10 +24,42 @@ class EditRobotTxt extends BaseModule
     /** @var string */
     const DOMAIN_NAME = 'editrobottxt';
 
-    /*
-     * You may now override BaseModuleInterface methods, such as:
-     * install, destroy, preActivation, postActivation, preDeactivation, postDeactivation
-     *
-     * Have fun !
+    /**
+     * @param ConnectionInterface|null $con
+     * @throws \Propel\Runtime\Exception\PropelException
      */
+    public function postActivation(ConnectionInterface $con = null)
+    {
+        try {
+            RobotsQuery::create()->findOne();
+        }catch (\Exception $e){
+            $database = new Database($con);
+            $database->insertSql(null, [__DIR__ . "/Config/thelia.sql"]);
+
+            $file = 'robots.txt';
+            if (file_exists($file)){
+                $current = file_get_contents($file);
+            }else{
+                $current = "";
+            }
+
+            $langQuery = LangQuery::create();
+
+            $langQuery
+                ->select(['url'])
+                ->setDistinct()
+                ->findByActive(true);
+
+            foreach ($langQuery->find() as $domain){
+                if ("" === $domain){
+                    $domain = $_SERVER['HTTP_HOST'];
+                }
+                $newRobots = new Robots();
+                $newRobots
+                    ->setDomainName($domain)
+                    ->setRobotsContent($current)
+                    ->save();
+            }
+        }
+    }
 }
